4c3867d386f7100190875cb5adf000e4eeadacc2
Clement VABRE <45317801+ClementVABRE@users.noreply.github.com>
2024-01-28 15:28:25 +0100

    Add files via upload

---
+++ arduino.cs
@@ -0,0 +1,29 @@
+#include <DHT.h>
+
+#define DHTPIN 2     // Définissez la broche de connexion du capteur
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Erreur de lecture du DHT11");
+    return;
+  }
+
+  Serial.print("Humidité: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Température: ");
+  Serial.print(t);
+  Serial.println(" *C");
+}
